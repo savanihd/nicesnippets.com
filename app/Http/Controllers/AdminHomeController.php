af18a9625da365f19b55eafdb4cffe34bdc14e34
rajeshlvi <rajesh.lalvadiya@virtueinfo.co>
2022-03-26 12:39:09 +0530

    add chanegs create issue resolved blog

--- app/Http/Controllers/AdminHomeController.php
+++ app/Http/Controllers/AdminHomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User; 
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 use App\Models\PostMethod;
 use App\Models\Tutorial;
 use App\Models\Blog;
