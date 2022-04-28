250c9b25a7f029aae9fc67edcc13c5427064511d
Hardik Savani <savanihd@gmail.com>
2022-04-28 15:34:25 +0530

    added change ads plce 4

--- resources/views/newBlogTheme/default.blade.php
+++ resources/views/newBlogTheme/default.blade.php
@@ -201,8 +201,8 @@
             </div>
             --}}
 
-            <div class="row content-box-new latest-posts">
-                <div class="col-md-12 hed-sub-text random-posts-section">
+            <div class="row content-box advertisement-box">
+                <div class="col-md-12 hed-sub-text">
                     <script type="text/javascript" src="//services.vlitag.com/adv1/?q=800ea5925dfc7c3a4aca0c649b41d8c3"></script><script> var vitag = vitag || {};</script>
         <div class="adsbyvli" data-ad-slot="outstream"></div> <script> vitag.outStreamConfig = { type: "slider", position: "right" }; </script>  
                 </div>
